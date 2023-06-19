3e62e77ea4419f691ad84bcd8e8bb69c434acd5f
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-06-19 21:04:19 +0500

    Add project files.

---
+++ Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pharmacy.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; } = string.Empty;
+        public DateTime CardExpiry { get; set; }
+        public string CVV { get; set; } = string.Empty;
+    }
+}
